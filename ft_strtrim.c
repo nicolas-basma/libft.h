fb320c9c24d1d748dad0e7c02b3bf8fcfdc1399b
Nicob11 <nicobasma_@hotmail.com>
2023-10-02 16:36:25 +0200

    ft_strtrim.c in process

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,34 +6,35 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:50:57 by febasma           #+#    #+#             */
-/*   Updated: 2023/09/29 17:56:41 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/02 16:35:25 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    unsigned int    i;
-    unsigned int    j;
-    char           *str;
+	int		i;
+	int		j;
+	char	*str;
 
-    i = 0;
-    j = 0;
-    str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
-    if (!s1 || !set)
-        return (NULL);
-    while(s1[i])
-    {
-        if(s1[i] == set[j])
-        {
-            str[i] = 'a';
-            i++;
-        }
-        str[i] = s1[i];
+	i = 0;
+	j = ft_strlen(s1) - 1;
+
+	if (!s1 || !set)
+		return (NULL);
+
+    while(s1[i] && ft_strchr(set, s1[i]))
         i++;
-    }
+
+    while(j > i && ft_strchr(set, s1[j]))
+        j--;
+
+    str = (char *)malloc(sizeof(char) * (j - i + 2));
+    if (!str)
+        return (NULL);
+
+    str = ft_substr(s1, i, j - i + 1);
     return (str);
-    
 }
 
--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include "libft.h"
 
-int main(int argc, char **argv)
-{;
-    char *p = ft_strtrim(argv[1], argv[2]);
+// int main(int argc, char **argv)
+// {
+//     char *p = ft_strtrim(argv[1], argv[2]);
+//     printf("%s", p);
+//     return (0);
+// }
+
+int main()
+{
+    char *str = "hello";
+    char *set = "he";
+    char *p = ft_strtrim(str, set);
     printf("%s", p);
     return (0);
 }
