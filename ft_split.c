f40e412e73311ff8b2f01948f7c689d82101f10f
Nico Basma <nicobasma_@hotmail.com>
2023-10-11 19:12:38 +0200

    split ready!!!

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:47:29 by febasma           #+#    #+#             */
-/*   Updated: 2023/10/10 20:08:46 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/11 19:11:39 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	number_words(char const *s, char c)
+int	ft_number_of_words(char const *s, char c)
 {
 	int	i;
 	int	old_i;
@@ -43,100 +43,36 @@ int	number_words(char const *s, char c)
 	return (count);
 }
 
-char	**cpy_to_split(char const *s, char c, int n)
+void	ft_word_len(char const *s, size_t *start, size_t *end, char c)
 {
-	int		i;
-	int		j;
-	int		len;
-	int		index;
-	char	temp[123456];
-	char	**str;
-
-	i = 0;
-	j = 0;
-	index = 0;
-	len = ft_strlen(s);
-	while (i < len)
-	{
-		while (i < len)
-		{
-			if (ft_strchr(&c, s[i]) == NULL)
-				break ;
-			i++;
-		}
-		while (i < len)
-		{
-			if (ft_strchr(&c, s[i]) != NULL)
-				break ;
-			temp[j] = s[i];
-			j++;
-			i++;
-		}
-		if (j > 0)
-		{
-			temp[j] = '\0';
-			str[index] = ft_strdup(temp);
-			index++;
-			j = 0;
-		}
-	}
-	return (str);
+	*start = *end;
+	while (s[*start] == c)
+		*start += 1;
+	*end = *start;
+	while (s[*end] != c && s[*end] != '\0')
+		*end += 1;
 }
 
 char	**ft_split(char const *s, char c)
 {
+	int 	i;
+	size_t	start;
+	size_t	end;
 	int		words;
 	char	**str;
 
-	words = number_words(s, c);
-	str = (char **)malloc(sizeof(char *) * (words + 1));
-	if (str == NULL)
-		return (NULL);
-	
-	return (str);
-}
-
-/*char	**ft_split(char const *s, char c)
-{
-	int i;
-	int j;
-	int words;
-	int index;
-	int len;
-	char temp[123456];
-	char **str;
-
 	i = 0;
-	j = 0;
-	index = 0;
-	words = number_words(s, c);
-	len = ft_strlen(s);
+	start = 0;
+	end = 0;
+	words = ft_number_of_words(s, c);
 	str = (char **)malloc(sizeof(char *) * (words + 1));
 	if (str == NULL)
 		return (NULL);
-	while (i < len)
+	while(i < words)
 	{
-		while (i < len)
-		{
-			if (ft_strchr(&c, s[i]) == NULL)
-				break ;
-			i++;
-		}
-		while (i < len)
-		{
-			if (ft_strchr(&c, s[i]) != NULL)
-				break ;
-			temp[j] = s[i];
-			j++;
-			i++;
-		}
-		if (j > 0)
-		{
-			temp[j] = '\0';
-			str[index] = ft_strdup(temp);
-			index++;
-			j = 0;
-		}
+		ft_word_len(s, &start, &end, c);
+		str[i] = ft_substr(s, start, end - start - 1);
+		i++;
 	}
 	return (str);
-}*/
+}
