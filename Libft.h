c9cdb1bcd0467c88787a22a0cdc70744ebdf226b
Nico Basma <nicobasma_@hotmail.com>
2023-10-17 19:44:34 +0200

    fix libft.h

--- Libft.h
+++ Libft.h
@@ -6,7 +6,7 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 16:32:18 by febasma           #+#    #+#             */
-/*   Updated: 2023/10/17 18:17:19 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/17 19:38:51 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *str, size_t n);
-int		ft_isalnum(char *str);
-int		ft_isalpha(char *str);
-int		ft_isascii(char *str);
-int		ft_isdigit(char *str);
-int		ft_isprint(char *str);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 void	*ft_memchr(void *str, int c, size_t n);
 void	ft_memcpy(void *dest, const void *src, size_t n);
 void	ft_memmove(void *dest, const void *src, int size);
