4e6d8f5bc30d5fdb592f05c7d5fe7c3e26b3c647
Nico Basma <nicobasma_@hotmail.com>
2023-10-20 18:46:23 +0200

    strlcat perfect

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 17:14:30 by febasma           #+#    #+#             */
-/*   Updated: 2023/10/19 22:02:05 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/20 18:29:42 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,16 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n)
 	size_t	i;
 	size_t	j;
 
-	i = 0;
 	j = 0;
-	i = ft_strlen(dest);
-	if (i >= n)
-		return (i + n);
-	while (src[j] != '\0' && i < n - 1)
-	{
-		dest[i] = src[j];
-		j++;
+	i = 0;
+	while (dest[i] && i < n)
 		i++;
+	while (src[j] && i + j + 1 < n)
+	{
+		dest[i + j] = src[j];
+		j++;
 	}
-	dest[i] = '\0';
-	return (i);
+	if (i != n)
+		dest[i + j] = '\0';
+	return (ft_strlen(src) + i);
 }
