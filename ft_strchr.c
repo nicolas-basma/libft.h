10e7542b77b40ed4584371fccbe8548af2b8553f
Nico Basma <nicobasma_@hotmail.com>
2023-10-23 17:29:47 +0200

    ft_strchr working well!!

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,31 +6,23 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 19:00:46 by febasma           #+#    #+#             */
-/*   Updated: 2023/10/10 18:57:05 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/23 17:29:02 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *str, int c)
 {
-	while (*str != '\0')
+	while (*str)
 	{
-		if (*str == c)
+		if (*str == (char)c)
 		{
 			return ((char *)str);
 		}
 		str++;
 	}
+	if (*str == (char)c)
+		return ((char *)str);
 	return (NULL);
 }
-
-// int main()
-// {
-// 	char str[] = "Hello World";
-// 	char c = 'l';
-// 	char *p = ft_strchr(str, c);
-// 	printf("%s", p);
-// 	return (0);
-// }
