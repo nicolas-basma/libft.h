6f40025b0760c7a14f49fa572ef1833db34bb998
Nico Basma <nicobasma_@hotmail.com>
2023-10-24 16:04:47 +0200

    memcmp fixed!

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 16:20:06 by febasma           #+#    #+#             */
-/*   Updated: 2023/10/19 19:40:46 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/24 16:02:57 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned int	i;
+	size_t			i;
 	unsigned char	*s1_cpy;
 	unsigned char	*s2_cpy;
 
@@ -29,13 +29,3 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
-
-/*int main()
-{
-    char str1[] = "Hello";
-    char str2[] = "Hello";
-    unsigned int n = 3;
-
-    printf("%d", ft_memcmp(str1, str2, n));
-    return (0);
-}*/
