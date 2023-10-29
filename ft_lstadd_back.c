b87ef91d1fcce3d75c9f15a8944fd5934fbfc442
Nico Basma <nicobasma_@hotmail.com>
2023-10-29 20:47:37 +0100

    ft_lstadd_back ok

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,11 +6,22 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 17:53:02 by febasma           #+#    #+#             */
-/*   Updated: 2023/10/29 17:53:20 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/29 20:45:34 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstadd_back(t_list **list, t_list *new)
-{}
+{
+	t_list	*last;
+	t_list	*before;
+
+	last = ft_lstnew(new->content);
+	if (list == NULL)
+		return ;
+	before = *list;
+	while (before->next != NULL)
+		before = before->next;
+	before->next = last;
+}
