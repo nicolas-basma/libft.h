677708ea0076a7dbf8c1ad922e812ffeaa7edc10
Nico Basma <nicobasma_@hotmail.com>
2023-10-31 11:18:54 +0100

    ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 17:54:06 by febasma           #+#    #+#             */
-/*   Updated: 2023/10/29 17:54:26 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/31 11:18:22 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,11 @@
 
 void	ft_lstiter(t_list *list, void (*f)(void *))
 {
+	if (!list || !f)
+		return ;
+	while (list)
+	{
+		(*f)(list->content);
+		list = list->next;
+	}
 }
