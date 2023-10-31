3a92e9a09b0781200d61af72d93995be12b4e155
Nico Basma <nicobasma_@hotmail.com>
2023-10-31 11:14:41 +0100

    ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,11 +6,23 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 17:53:27 by febasma           #+#    #+#             */
-/*   Updated: 2023/10/29 17:53:42 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/31 11:14:05 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstclear(t_list **list, void (*del)(void *))
-{}
+{
+	t_list *tmp;
+
+	if (!list || !del)
+		return ;
+	while (*list)
+	{
+		tmp = (*list)->next;
+		ft_lstdelone(*list, del);
+		*list = tmp;
+	}
+	free(*list);
+}
