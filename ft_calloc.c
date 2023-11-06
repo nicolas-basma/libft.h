1614f4eb9a298df563bec88c07f40e535fea05ac
Nico Basma <nicobasma_@hotmail.com>
2023-11-06 21:44:43 +0100

    ft_calloc after libft ok!

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 18:12:40 by febasma           #+#    #+#             */
-/*   Updated: 2023/10/25 23:56:38 by febasma          ###   ########.fr       */
+/*   Updated: 2023/11/06 21:43:06 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nitems, size_t size)
 	void	*ptr;
 
 	ptr = malloc(nitems * size);
-	if (ptr == NULL)
+	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, nitems * size);
 	return (ptr);
